cc29c6daee5a100dc90227c6488b00fe0de79b7d
Divyanshu Singh <69934570+RaptorDVZ@users.noreply.github.com>
2021-04-12 05:19:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 
 client= commands.Bot(command_prefix = '>')
 
-bad_words=['fuck','bhosadike','madarchod','gandu','chutiye','jhaatu','jhatu','chut','loda','lauda','lawda','motherfucker','asshole','shithole','pussy','cunt','lode','lawde','lund','behenchod','banchod','benchod','benchoda']
+bad_words=['fuck','bhosadike','madarchod','gandu','chutiye','jhaatu','jhatu','chut','loda','lauda','lawda','motherfucker','asshole','shithole','cunt','lode','lawde','lund','behenchod','banchod','benchod','benchoda','chod','chuda','ass','dick','chud']
 
 response='WARNING :You cannot use this word here '
 
@@ -26,11 +26,6 @@ async def on_message(message):
   msg = message.content
   
   
-  if msg.startswith('>hi'):
-    embedVar = discord.Embed(title="HELLO THERE", description=mention, color=0x00ff00)
-    #embedVar.add_field(name="Field1", value="hi", inline=False)
-    #embedVar.add_field(name="Field2", value="hi2", inline=False)
-    await message.channel.send(embed=embedVar)
 
   if any(word in msg.lower() for word in bad_words):
     await message.channel.purge(limit=1)
@@ -41,8 +36,27 @@ async def on_message(message):
   await client.process_commands(message)
 
 @client.command()
+@commands.has_permissions(manage_messages=True)
 async def clear(ctx,amount=5):
   await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def hi(ctx):
+  embedVar = discord.Embed(title="HELLO THERE", description=ctx.author.mention, color=0x00ff00)
+  #embedVar.add_field(name="Field1", value="hi", inline=False)
+  #embedVar.add_field(name="Field2", value="hi2", inline=False)
+  await ctx.channel.send(embed=embedVar)
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def warn(ctx,member,message):
+  embed = discord.Embed(title="WARNING!!", description=member, color=0x00ff00)
+  
+  embed.add_field(name="Reason:", value=message, inline = False)
+  #embedVar.add_field(name="Reason", value="hi2", inline=False)
+  await ctx.channel.purge(limit=1)
+  await ctx.channel.send(embed=embed)
+
 
 
 alive()
